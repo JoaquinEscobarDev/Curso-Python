e55f76bb24a9b1b09429d8ff61f62e3b38f71464
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-01-28 14:25:43 -0300

    clase 1

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,28 @@
-print("Hello world")
+
+nombre = 'Joaquin Andres'
+last_name = 'Escobar Letelier'
+caracter = "c"
+
+
+
+
+#print("Hello world")
+#print(1+1)
+#print(nombre + ' (hola) ' + last_name)
+# print(type(nombre))
+# print(caracter)
+#print(len(nombre))
+#print(len(last_name))
+# print(nombre.lower())
+# print(nombre.upper())
+# print(nombre.strip())
+
+#print(nombre * 5)
+# print(nombre[0])
+# print(nombre[1])
+# print(nombre[2])
+# print(nombre[3])
+# print(nombre[4])
+# print(nombre[5])
+# print(nombre[6])
+
