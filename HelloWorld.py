ad28ce4c1442aa0bbe961bcb48c910e5722283f9
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-01-28 14:49:14 -0300

    uso del print

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,6 +3,31 @@ nombre = 'Joaquin Andres'
 last_name = 'Escobar Letelier'
 caracter = "c"
 
+#uso de sep=", " coloca todo lo que esta despues del = como separador
+print("Hola", "Mundo", sep=", ")
+
+#uso de end hace que aunque se tenga un salto de linea, se imprima al lado
+print("Hola", end=" ")
+print("Mundo")
+
+#formate f-string permite colocar variables dentro de las llaves
+frase = "hola mundo"
+autor = "yo"
+print(f"Frase:  {frase}, Autor: {autor}")
+
+#formato con format las llaves se usan como marcador de posicion que se agregan en el orden dentro del .format
+print("Frase: {}, Autor: {}".format(frase, autor))
+
+#formato especifico dentro de las llaves {:.2f} se indica que debe tener 2 numeros despues del .
+valor = 3.14167
+print("Valor:  {:.2f}".format(valor))
+
+#Salto de linea con \
+#para evitar error de sintaxis al utilizar comillas en una cadena de texto se ocupa 'hola \'algo\''
+#para colocar una ruta debes repetir los \ asi \\
+
+#
+
 
 
 
