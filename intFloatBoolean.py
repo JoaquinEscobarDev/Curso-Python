c88f4d5e73050da05f6db2e99a897eeae2a266da
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-01-28 15:03:43 -0300

    operaciones numericas

--- intFloatBoolean.py
+++ intFloatBoolean.py
@@ -1,14 +1,24 @@
-x = 10
-y = 5.678
-z = 1.2E6
-a = 1.2E-6
+# x = 10
+# y = 5.678
+# z = 1.2E6
+# a = 1.2E-6
 
 
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# print(type(a))
+# print(x + x)
 
+#Operaciones numericas
 
-print(type(x))
-print(type(y))
-print(type(z))
-print(type(a))
-print(x + x)
+a = 10
+b = 3 
 
+print("Suma: ", a + b)
+print("Resta: ", a - b)
+print("Multiplicacion: ", a * b)
+print("Division: ", a / b)
+print("Modulo: ", a % b)
+print("Potencia: ", a ** b)
+print("Parte entera de la division: ", a // b)
