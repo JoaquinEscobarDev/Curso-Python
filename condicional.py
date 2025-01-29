817be0d49721d7fb78a9a4588b584ae5c20962c4
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-01-28 21:05:51 -0300

    Uso de condicionales

---
+++ condicional.py
@@ -0,0 +1,27 @@
+x = 10
+y = 20
+
+if x > 5:
+    print("X es mayor que 5")
+elif x == 5:
+    print("X es igual a 5")
+else:
+    print("X no es mayor que 5")
+
+if x>10 and y>20:
+    print("X es mayor que 10 y Y es mayor que 20")
+else: 
+    print("X es igual a 10 y Y es igual a 20")
+
+if x < 15 or y < 25:
+    print("X es menor que 15 o Y es menor que 25")
+else:
+    print("X no es menor que 15 y Y no es menor que 25")
+if not (x == 10):
+    print("X no es igual a 10")
+else:
+    print("X es igual a 10")
+if x != 10:
+    print("X no es igual a 10")
+else:
+    print("X es igual a 10")
