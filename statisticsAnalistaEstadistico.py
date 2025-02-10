87ecddcd4acd6e939991faf49a16c486950728e9
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-02-10 20:25:30 -0300

    hallar la moda

--- statisticsAnalistaEstadistico.py
+++ statisticsAnalistaEstadistico.py
@@ -19,3 +19,8 @@ print(sales)
 mean_sales = statistics.mean(sales)
 print(f"La media es: {mean_sales}")
 
+#--------------------------------------------------------------
+#Hallar la moda
+
+mean_sales = statistics.mode(sales)
+print(f"La moda es: {mean_sales}")
