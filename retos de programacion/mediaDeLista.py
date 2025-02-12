5a9895bca667fc8014db07b4eaa36331fc315d7b
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-02-11 23:52:03 -0300

    Reto de programacion 2

---
+++ retos de programacion/mediaDeLista.py
@@ -0,0 +1,11 @@
+"""
+MEDIA 
+Calcula la media de un listado de números.
+"""
+import statistics
+
+lista = [1,5,6,4,8,8,7,5,4,1,3,9,8,1,0]
+
+media = statistics.mean(lista)
+print(f"La media es: {media}")
+
