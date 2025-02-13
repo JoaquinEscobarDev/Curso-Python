b4e37cd29e28b3a09fecace23eeac39e1c325bd4
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-02-13 16:11:26 -0300

    promedio con buenas practica, codigo pythoniano

---
+++ promedio.py
@@ -0,0 +1,25 @@
+# def calculate_avarage(numbers):
+#     """
+#     Calcula el promedio de una lista e numeros.
+    
+#     Parametros:
+#     numbers (list): una lista de numeros enteros o flotantes
+
+#     Retorna:
+#     float: El promedio de los numeros en la lista
+#     """
+#     return sum(numbers) / len(numbers)
+
+# print(calculate_avarage([1,2,3,4,5,6]))
+
+
+def calculate_area(base,height):
+    """
+    Calcula el area de un triangulo
+
+    """
+    #Multiplica la base por la altura y luego la diviimos entre dos
+    return (base * height) / 2
+    
+
+    
