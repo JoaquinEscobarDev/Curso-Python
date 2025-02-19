d660a810387742198358fcb947e3d41afc9ad355
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-02-18 22:54:12 -0300

    manejo de validacion con try

--- fundamentosAvanzados/divide_1.py
+++ fundamentosAvanzados/divide_1.py
@@ -1,13 +1,25 @@
 def divide(a:int, b: int) -> float:
     if not isinstance(a,int) or not isinstance(b, int):
-        print('Error: Ambos parametros deben ser enteros o flotantes')
-        return None
+        raise TypeError('Ambos parametros deben ser enteros')
+        
+        # print('Error: Ambos parametros deben ser enteros o flotantes')
+        # return None
     #Verificamos que el divisor no sea 0
     if b==0:
-        print('El divisor no puede ser 0')
-        return None
+        raise TypeError('El divisor no puede ser 0')
+        
+        # print('El divisor no puede ser 0')
+        # return None
 
     return a/b
+
+try: 
+    res = divide(10,'2') #Error de tipo
+    print(res)
+except TypeError as e:
+    print(f'Error: {e}')
+    
+
 
 res1 = divide(10,2)
 res2 = divide(10,0)
