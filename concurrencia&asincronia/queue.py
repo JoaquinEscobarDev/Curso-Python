bdb216b54712d48dcda3cb838cae32e7e9469e98
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-02-21 14:11:58 -0300

    agrega funcionalidad para calcular el cuadrado de números utilizando multiprocessing y una cola para la comunicación entre procesos

---
+++ concurrencia&asincronia/queue.py
@@ -0,0 +1,19 @@
+#Usamos Queue para que el proceso secundario pueda pasar datos de vuelta al proceso principal.
+
+import multiprocessing
+
+def calcular_cuadrado(numeros, cola):
+    for n in numeros:
+        cola.put(n * n)
+
+if __name__ == "__main__":
+    numeros = [1, 2, 3, 4, 5]
+    cola = multiprocessing.Queue()
+
+    proceso = multiprocessing.Process(target=calcular_cuadrado, args=(numeros, cola))
+    proceso.start()
+    proceso.join()
+
+    # Extraer resultados de la cola
+    while not cola.empty():
+        print(cola.get())
