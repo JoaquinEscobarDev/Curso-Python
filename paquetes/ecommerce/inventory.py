5496f411383f784050af2ce4782d16398475e434
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-02-22 00:06:04 -0300

    agrega funciones para gestionar inventario y procesar ventas

---
+++ paquetes/ecommerce/inventory.py
@@ -0,0 +1,8 @@
+# Agrega un producto al inventario
+def add_product(product_name, stock):
+    print(f'Producto {product_name} agregado con stock de: {stock}')
+    
+#Elimina un producto del inventario
+def remove_product(product_name):
+    print(f'Producto {product_name}. Eliminado del inventario')
+
